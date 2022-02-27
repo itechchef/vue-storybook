9120fa0e81974176e0a6ffe5c19cf8565a7c11a5
itechchef <infotechchef@gmail.com>
2022-02-27 06:18:34 -0800

    Add dropdown to navbar

--- src/components/Navigation/Dropdown/Dropdown.vue
+++ src/components/Navigation/Dropdown/Dropdown.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="inline-block" @click.stop>
+  <div class="inline-block relative" @click.stop>
     <component
       :is="navmenu ? 'a' : 'button'"
       :class="dropdownClass"
       @click="showList = !showList"
     >
-      {{ text }}
+      <slot name="menu-content">
+        {{ text }}
+      </slot>
       <svg v-if="!navmenu" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
       </svg>
     </component>
 
-    <ul v-if="showList" class="text-cool-gray-500 bg-white drop-shadow flex flex-col flex-nowrap gap-1 rounded-md overflow-hidden list-none min-w-[12.25rem] mt-1 p-0 dark:text-cool-gray-300 dark:bg-cool-gray-700"><slot /></ul>
+    <ul v-if="showList" :class="listClass"><slot /></ul>
   </div>
 </template>
 
@@ -31,6 +33,9 @@ export default {
     className: {
       type: [String, Object],
     },
+    listClassName: {
+      type: [String, Object],
+    },
     text: {
       type: String,
       default: "",
@@ -39,7 +44,8 @@ export default {
   computed: {
     dropdownClass() {
       let classes = {
-        "bg-blue-600 text-white text-base font-medium flex items-center gap-1 justify-center py-3 px-4 rounded-lg": !this.navmenu,
+        "flex items-center gap-1 justify-center": true,
+        "bg-blue-600 text-white text-base font-medium py-3 px-4 rounded-lg": !this.navmenu,
         "bg-transparent cursor-default": this.navmenu,
       };
 
@@ -51,6 +57,19 @@ export default {
 
       return classes;
     },
+    listClass() {
+      let classes = {
+        "text-cool-gray-500 bg-white drop-shadow flex flex-col flex-nowrap gap-1 rounded-md overflow-hidden list-none min-w-[12.25rem] mt-1 p-0 dark:text-cool-gray-300 dark:bg-cool-gray-700 absolute bottom-0 translate-y-full": true,
+      };
+
+      if (typeof this.listClassName === "string") {
+        classes = { ...classes, [this.listClassName]: true };
+      } else if (typeof this.listClassName === "object") {
+        classes = { ...classes, ...this.listClassName };
+      }
+
+      return classes;
+    }
   },
   mounted() {
     const _instance = this;
--- src/components/Navigation/Navbar/Navbar.vue
+++ src/components/Navigation/Navbar/Navbar.vue
@@ -45,7 +45,7 @@ export default {
   computed: {
     navbarClass() {
       let classes = {
-        "text-sm font-medium bg-white border-cool-gray-200 px-2 sm:px-4 py-2.5 w-full h-16 flex flex-nowrap gap-4 items-center justify-between": true,
+        "text-sm font-medium bg-white border-cool-gray-200 px-2 sm:px-4 py-2.5 w-full h-16 flex flex-nowrap gap-4 items-center justify-between dark:text-cool-gray-300 dark:bg-cool-gray-800": true,
         "text-cool-gray-500": this.color == "white",
         "!text-cool-gray-300 !bg-cool-gray-800": this.color == "dark",
       }
